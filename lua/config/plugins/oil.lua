04522c420ab1d68ae14be418d30d15e6d638cac3
George Murga <george.murga+github@gmail.com>
2024-12-08 17:28:04 +0200

    More stuff

---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    enabled = true,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function ()
+      require("oil").setup({
+	columns = { "icon" },
+	keymaps = {
+	  ["<C-h>"] = false,
+	  ["<M-h>"] = { "actions.select", opts = { horizontal = true } },
+	},
+	view_options = {
+	  show_hidden = true,
+	},
+      })
+
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  },
+}
