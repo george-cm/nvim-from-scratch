a7b31437aea8cc52eb8f3c9a5aaac8c6dda689e4
George Murga <george.murga+github@gmail.com>
2025-01-05 19:55:58 +0200

    Add a couple of mini.nvim plugins and which-key

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -3,9 +3,11 @@ return {
   {
     'echasnovski/mini.nvim',
     enabled = true,
+    version = false,
     config = function()
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
+      require('mini.ai').setup({})
     end
   },
 }
